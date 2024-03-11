9e450d43bf06e01f78e37ac1871d86c934588de8
@hexlet/cli <support@hexlet.io>
2024-03-11 11:19:03 +0300

    submit java-web-ru/handlers

---
+++ java-web-ru/handlers/src/main/java/exercise/App.java
@@ -0,0 +1,28 @@
+package exercise;
+
+import io.javalin.Javalin;
+
+import java.io.InputStream;
+import java.util.List;
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        // BEGIN
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+        List<String> phones = Data.getPhones();
+        List<String> domains = Data.getDomains();
+        app.get("/phones", ctx -> ctx.json(phones));
+        app.get("/domains", ctx -> ctx.json(domains));
+        return app;
+        // END
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
