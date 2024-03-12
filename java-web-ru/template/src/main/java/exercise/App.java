5b520da0541f5ced80dfc3db1bec90ea7f914052
@hexlet/cli <support@hexlet.io>
2024-03-12 14:59:23 +0300

    submit java-web-ru/template

---
+++ java-web-ru/template/src/main/java/exercise/App.java
@@ -0,0 +1,57 @@
+package exercise;
+
+import io.javalin.Javalin;
+import java.util.List;
+import io.javalin.http.NotFoundResponse;
+import exercise.model.User;
+import exercise.dto.users.UserPage;
+import exercise.dto.users.UsersPage;
+import java.util.Collections;
+import java.util.Optional;
+
+public final class App {
+
+    // Каждый пользователь представлен объектом класса User
+    private static final List<User> USERS = Data.getUsers();
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        // BEGIN
+        app.get("/users/{id}", ctx -> {
+            var userId = ctx.pathParam("id");
+
+            Optional<User> us = USERS.stream()
+                    .filter(c -> c.getId() == (Integer.parseInt(userId)))
+                    .findFirst();
+
+            var user = new UserPage(us.get());
+
+            if (us.isEmpty()) {
+                throw new NotFoundResponse("User not found");
+            }
+            ctx.render("users/show.jte", Collections.singletonMap("user", user));
+        });
+
+        app.get("/users", ctx -> {
+            var page = new UsersPage(USERS);
+
+            ctx.render("users/index.jte", Collections.singletonMap("page", page));
+        });
+        // END
+
+        app.get("/", ctx -> {
+            ctx.render("index.jte");
+        });
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
